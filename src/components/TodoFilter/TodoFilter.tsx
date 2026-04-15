c1c304d102416ae406242d8b002cc099b04ab467
Andrey <kagamlykas@gmail.com>
2026-04-15 14:59:41 +0300

    Created list with store

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { filterSlice } from '../../features/filter';
+import { Status } from '../../types/Status';
+import debounce from 'lodash.debounce';
 
 export const TodoFilter: React.FC = () => {
+  const { query, status } = useAppSelector(state => state.filter);
+  const dispatch = useAppDispatch();
+
+  const [tempQuery, setTempQuery] = useState('');
+  const setResidualQuery = useCallback(
+    debounce(
+      currentQuery => dispatch(filterSlice.actions.setQuery(currentQuery)),
+      400,
+    ),
+    [],
+  );
+
   return (
-    <form
-      className="field has-addons"
-      onSubmit={event => event.preventDefault()}
-    >
+    <form className="field has-addons">
       <p className="control">
         <span className="select">
-          <select data-cy="statusSelect">
+          <select
+            data-cy="statusSelect"
+            onChange={event =>
+              dispatch(
+                filterSlice.actions.setStatus(event.target.value as Status),
+              )
+            }
+            value={status}
+          >
             <option value="all">All</option>
             <option value="active">Active</option>
             <option value="completed">Completed</option>
@@ -22,19 +43,30 @@ export const TodoFilter: React.FC = () => {
           type="text"
           className="input"
           placeholder="Search..."
+          onChange={event => {
+            setTempQuery(event.target.value);
+            setResidualQuery(event.target.value);
+          }}
+          value={tempQuery}
         />
         <span className="icon is-left">
           <i className="fas fa-magnifying-glass" />
         </span>
 
-        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button
-            data-cy="clearSearchButton"
-            type="button"
-            className="delete"
-          />
-        </span>
+        {query && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={() => {
+                dispatch(filterSlice.actions.reset());
+                setTempQuery('');
+              }}
+            />
+          </span>
+        )}
       </p>
     </form>
   );
